855524ab937bc1a9311907a5cff0b05a84ffda96
Vishnu Ghule <Vishnu.Ghule@matrixhealth.net>
2019-05-03 22:02:32 -0700

    intial commit

---
+++ gitevents-api/src/main/java/com/github/events/giteventsapi/GiteventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.events.giteventsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiteventsApiApplication.class, args);
+	}
+
+}
