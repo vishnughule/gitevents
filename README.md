380e9add357de12d6b408a4625eadf3b1404e8f9
Vishnu Ghule <Vishnu.Ghule@matrixhealth.net>
2019-05-05 17:53:33 -0700

    Added more test cases and updated Readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# GiteventsUi
+
+## Build
+
+To run this project you will need to have maven version of atleast 3.1.0
+
+to build this project run  mvn clean install on root of the project gitevents
+
+Its good to have Node and npm installed on your machine, if Node and npm not installed it will try to install them as part of maven build process.
+
+Once project is successfully Built , Navigate to gitevents/gitevnets-api/target and run below command
+
+java -jar gitevents-api-0.0.1-SNAPSHOT.jar
+
+try localhost:8080 and main page should appear
+
+
+## Running unit tests
+
+Run mvn test on gitevnts-api project 
+
+
+## Further help
+
--- gitevents-api/src/test/java/com/github/events/giteventsapi/GitEventsServiceTests.java
+++ gitevents-api/src/test/java/com/github/events/giteventsapi/GitEventsServiceTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -15,8 +16,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import com.github.events.giteventsapi.dto.Actor;
 import com.github.events.giteventsapi.dto.Event;
 import com.github.events.giteventsapi.service.EventService;
 import com.github.events.giteventsapi.service.EventServiceImpl;
@@ -30,16 +35,99 @@ public class GitEventsServiceTests {
 	@InjectMocks
 	private EventService empService = new EventServiceImpl();
 
+	/**
+	 * Check if empty response is coming back in response
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void eventsServiceShouldReturnEmpty() throws Exception {
-		
+
 		ResponseEntity<List<Event>> response = new ResponseEntity<List<Event>>(new ArrayList<Event>(), HttpStatus.OK);
-		
-		when(restTemplate.exchange("https://api.github.com/repos/vishnughule/gitevents/events?page=1", HttpMethod.GET, null,  new ParameterizedTypeReference<List<Event>>() {
-		})).thenReturn(response); 
-		
-		assertEquals(0,  empService.getEvents("vishnughule", "gitevents", "PushEvent").size());
-		
+
+		when(restTemplate.exchange("https://api.github.com/repos/vishnughule/gitevents/events?page=1", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Event>>() {
+				})).thenReturn(response);
+
+		assertEquals(0, empService.getEvents("vishnughule", "gitevents", "PushEvent").size());
+
+	}
+
+	/**
+	 * Test to check if the filter by event type is working
+	 */
+
+	@Test
+	public void eventServiceToCheckFilter() throws Exception {
+		Actor actor = new Actor("vishnu.ghule", "vishnughule");
+		Event event = new Event("PushEvent", new Date(), actor);
+		List<Event> events = new ArrayList<Event>();
+		events.add(event);
+		Event event2 = new Event("WatchEvent", new Date(), actor);
+		events.add(event2);
+
+		ResponseEntity<List<Event>> response = new ResponseEntity<List<Event>>(events, HttpStatus.OK);
+		when(restTemplate.exchange("https://api.github.com/repos/vishnughule/gitevents/events?page=1", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Event>>() {
+				})).thenReturn(response);
+
+		List<Event> resultEvents = empService.getEvents("vishnughule", "gitevents", "PushEvent");
+		assertEquals(1, resultEvents.size());
+		assertEquals("PushEvent", resultEvents.get(0).getType());
+
+	}
+
+	/**
+	 * Test to check if callig the api for next page
+	 * 
+	 * @throws Exception
+	 */
+
+	@Test
+	public void eventServiceToCheckifTwoPages() throws Exception {
+		Actor actor = new Actor("vishnu.ghule", "vishnughule");
+		Event event = new Event("PushEvent", new Date(), actor);
+		List<Event> eventsPage1 = new ArrayList<Event>();
+		eventsPage1.add(event);
+		Event event2 = new Event("PushEvent", new Date(), actor);
+
+		List<Event> eventsPage2 = new ArrayList<Event>();
+		eventsPage2.add(event2);
+
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+		headers.add("Link", "\"<https://api.github.com/repos/vishnughule/gitevents/events?page=2>; rel=\"last\"");
+
+		ResponseEntity<List<Event>> responsePage1 = new ResponseEntity<List<Event>>(eventsPage1, headers, HttpStatus.OK);
+
+		when(restTemplate.exchange("https://api.github.com/repos/vishnughule/gitevents/events?page=1", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Event>>() {
+				})).thenReturn(responsePage1);
+
+		ResponseEntity<List<Event>> responsePage2 = new ResponseEntity<List<Event>>(eventsPage2, HttpStatus.OK);
+		when(restTemplate.exchange("https://api.github.com/repos/vishnughule/gitevents/events?page=2", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Event>>() {
+				})).thenReturn(responsePage2);
+
+		List<Event> resultEvents = empService.getEvents("vishnughule", "gitevents", "PushEvent");
+		assertEquals(2, resultEvents.size());
+
+	}
+
+	@Test
+	public void eventServiceNotFoundException() throws Exception {
+
+		when(restTemplate.exchange("https://api.github.com/repos/vishnughule/gitevents/events?page=1", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Event>>() {
+				})).thenThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
+
+		try {
+			empService.getEvents("vishnughule", "gitevents", "PushEvent");
+		}
+		catch (CustomException cex) {
+			assertEquals(new CustomException(HttpStatus.NOT_FOUND, "Please check owner and repo, no events found for this combination").getErrorCode(),
+					cex.getErrorCode());
+		}
+
 	}
 
 }
